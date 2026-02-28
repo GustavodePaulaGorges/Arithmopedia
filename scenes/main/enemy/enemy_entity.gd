9bfca5764141ba19cf44cd2718e156a18bf904d8
Gustavo P. Gorges <Gustavodep.gorges@gmail.com>
2026-02-28 15:01:37 -0300

    Inimigos agora seguem um Path2D fixo
    O spawner de inimigos agora recebe um caminho e instancia os inimigos com PathFollow abaixo desse caminho, fazendo com que o sigam.

--- scenes/main/enemy/enemy_entity.gd
+++ scenes/main/enemy/enemy_entity.gd
@@ -2,33 +2,12 @@ class_name EnemyEntity
 extends CharacterBody2D
 
 @export var movement_speed : float = 200
-@export var path_finding_manager : PathfindingManager = null
-@export var target_pos : Marker2D = null
 
-var path_array : Array[Vector2i] = []
+var path_follow: PathFollow2D
 
 func _ready() -> void:
-	print(path_finding_manager)
-	path_array = path_finding_manager.get_valid_path(global_position / 16, target_pos.position / 16)
-	print("PATH:", path_array)
+	path_follow = get_parent() as PathFollow2D
 
 func _physics_process(delta: float) -> void:
-	get_path_to_postion()
-	move_and_slide()
-
-func get_path_to_postion() -> void:
-	if path_array.is_empty():
-		velocity = Vector2.ZERO
-		return
-
-	var target_position : Vector2 = path_array[0]
-	var distance : float = global_position.distance_to(target_position)
-	var step : float = movement_speed * get_process_delta_time()
-
-	if distance <= step:
-		global_position = target_position
-		path_array.remove_at(0)
-		velocity = Vector2.ZERO
-	else:
-		var direction : Vector2 = global_position.direction_to(target_position)
-		velocity = direction * movement_speed
+	if path_follow:
+		path_follow.progress += movement_speed * delta
--- scenes/main/spawners/enemy_spawner.gd
+++ scenes/main/spawners/enemy_spawner.gd
@@ -4,6 +4,9 @@ extends Node2D
 signal spawning_phase_complete
 
 @export var wave_data_array : Array = ["Enemy 1", "Enemy2", "Enemy3", "Enemy4"]
+@export var path2D : Path2D
+@onready var enemy_container = $EnemyContainer
+@export var enemy_scene: PackedScene
 @onready var enemy_spawn_timer: Timer = $EnemySpawnTimer
 
 const DEFAULT_SPAWN_DELAY : float = 1.0
@@ -18,10 +21,18 @@ func _ready() -> void:
 
 func spawn_entity() -> void:
 	if current_data_index >= wave_data_array.size():
-		printerr("Wave data array empty")
 		enemy_spawn_timer.stop()
 		spawning_phase_complete.emit()
 		return
+
+	var path_follow := PathFollow2D.new()
+	path_follow.loop = false
+	path_follow.rotates = false
+	path_follow.progress_ratio = 0.0
+	path2D.add_child(path_follow)
+
+	var enemy = enemy_scene.instantiate()
+	path_follow.add_child(enemy)
 
 func _on_enemy_spawn_timer_timeout() -> void:
 	spawn_entity()
