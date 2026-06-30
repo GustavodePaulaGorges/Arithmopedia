6e3f635e3a30638584b25cc9dca95a7c02af1fb2
GustavodePaulaGorges <gustavodep.gorges@gmail.com>
2026-06-29 21:17:23 -0300

    Adicionando dicas nas fases

---
+++ scenes/default_stage/modals/hint_modal.gd
@@ -0,0 +1,23 @@
+extends Control
+
+@onready var hint_text: Label = $CenterContainer/Panel/VBoxContainer/HintText
+@onready var ok_button: Button = $CenterContainer/Panel/VBoxContainer/OKButton
+
+func _ready():
+	get_tree().root.print_tree_pretty()
+	ok_button.pressed.connect(_on_ok_button_pressed)
+
+func _on_ok_button_pressed():
+	hide_modal()
+
+func show_modal():
+	visible = true
+	get_tree().paused = true
+
+func hide_modal():
+	visible = false
+	get_tree().paused = false
+
+func set_stage_info(hint: String):
+	if hint_text:
+		hint_text.text = hint
